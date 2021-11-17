b2286dc11cc1c0a9490daf27309eff379de11cb5
Rezzak <errazakallah3190@gmail.com>
2021-11-17 15:32:46 +0100

    first commit | project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bejeweled_blitz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
