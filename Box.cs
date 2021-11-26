de6ae7f714b24cdd6ddb432376abe19609bb26e1
Rezzak <errazakallah3190@gmail.com>
2021-11-26 04:06:41 +0100

    Settings class added

--- Box.cs
+++ Box.cs
@@ -4,98 +4,25 @@ using System.Text;
 
 namespace Bejeweled_blitz
 {
-    public class Map
+    public class Box
     {
-        private static ConsoleColor[] colors = { ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Cyan };
-        private Random rnd = new Random();
-        private int x;
-        private int y;
-        private bool isSelected;
-        private bool isCursorPosition;
-        private ConsoleColor color;
+        public int x;
+        public int y;
+        public ConsoleColor color;
+        public bool isSelected;
+        public bool isCursorPosition;
         private char[,] symbols = new char[3, 3];
-        private Map[,] playField = new Map[8, 8];
 
-        public ConsoleColor Color
-        {
-            get
-            {
-                return color;
-            }
-            set
-            {
-                if (value == ConsoleColor.Black)
-                {
-                    //Check if that really works
-                    color = value;
-                }
-            }
-        }
-
-        public int X
-        {
-            get
-            {
-                return x;
-            }
-            set
-            {
-                x = value;
-            }
-        }
-
-        public int Y
-        {
-            get
-            {
-                return x;
-            }
-            set
-            {
-                x = value;
-            }
-        }
-
-        public bool IsSelected
-        {
-            get
-            {
-                return isSelected;
-            }
-            set
-            {
-                isSelected = value;
-            }
-        }
-
-        public bool IsCursorPosition
-        {
-            get
-            {
-                return isCursorPosition;
-            }
-            set
-            {
-                isCursorPosition = value;
-            }
-        }
-
-        //public Map(int x, int y, ConsoleColor color)
-        public Map(int x, int y, ConsoleColor color)
+        public Box(int x, int y, ConsoleColor color)
         {
             this.x = x;
             this.y = y;
+            this.color = color;
             this.isSelected = false;
             this.isCursorPosition = false;
-            this.color = color;
         }
 
-        public Map() : this(0, 0, colors[0])
-        {
-        }
-
-        // hadi ri katchad kola element fl array wkatstori fih symbol li kayjiha ka parameter (khaso ywali mli t3ayat l class)
-        public void InitOneJewelry(char symbol)
+        public void InitBox(char symbol)
         {
             for (int i = 0; i < symbols.GetLength(0); i++)
             {
@@ -106,40 +33,38 @@ namespace Bejeweled_blitz
             }
         }
 
-        //katchad kola element bo7do wta5do b position dyalo wtsaba4lo sub element li fosto lda5al bach ta3tina one box
-        public void drawBox()
+        public void DrawBox()
         {
-            //Color kay3ni l color li tpasa mli t3ayat 3la had l class ka parameter
             Console.ForegroundColor = this.color;
-            for (int i = 0; i < symbols.GetLength(0); i++)
+            for (int i = 0; i < this.symbols.GetLength(0); i++)
             {
-                for (int j = 0; j < symbols.GetLength(1); j++)
+                for (int j = 0; j < this.symbols.GetLength(1); j++)
                 {
                     Console.SetCursorPosition(this.x + i, this.y + j);
                     Console.Write(this.symbols[i, j]);
+                    //Console.Write('T');
                 }
             }
-            //TODO: Maybe 5asni n7atha f function bo7dhom hado
-            // is seledected 3naw biha mli tkon wati 3la espace tma katkon selected
+            //TODO: DYALI HADI MASAL7A LTA 9ALWA 5ASNI NA
             switch (this.isSelected)
             {
                 case false: // Not Selected
-                    //Console.SetCursorPosition(this.x + 1, this.y - 1);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 3, this.y + 1);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 3, this.y);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 3, this.y + 2);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 1, this.y + 3);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x - 1, this.y + 1);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x - 1, this.y);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x - 1, this.y + 2);
-                    //Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 1, this.y - 1);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 3, this.y + 1);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 3, this.y);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 3, this.y + 2);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 1, this.y + 3);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x - 1, this.y + 1);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x - 1, this.y);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x - 1, this.y + 2);
+                    Console.Write(' ');
                     break;
 
                 case true: // isSelected
@@ -162,14 +87,14 @@ namespace Bejeweled_blitz
             switch (isCursorPosition)
             {
                 case false: // Not Selected
-                    //Console.SetCursorPosition(this.x - 1, this.y - 1);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 3, this.y - 1);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x + 3, this.y + 3);
-                    //Console.Write(' ');
-                    //Console.SetCursorPosition(this.x - 1, this.y + 3);
-                    //Console.Write(' ');
+                    Console.SetCursorPosition(this.x - 1, this.y - 1);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 3, this.y - 1);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x + 3, this.y + 3);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(this.x - 1, this.y + 3);
+                    Console.Write(' ');
                     break;
 
                 case true: // isSelected
@@ -184,23 +109,6 @@ namespace Bejeweled_blitz
                     Console.Write('\u2514');
                     break;
             }
-        }
-
-        // kaysna3 lfield kaml by making box by box, kola wa7d m5talaf 3la lakhor by y3tihom kola mra location dyal box w symbol li brah tma wlkhra l color li yfilih
-        public Map[,] InitField()
-        {
-            for (int i = 0; i < playField.GetLength(0); i++)
-            {
-                for (int j = 0; j < playField.GetLength(1); j++)
-                {
-                    playField[i, j] = new Map(i * 4 + 1, j * 4 + 1, colors[rnd.Next(0, colors.Length)]);
-                    // Dark-Shade: \u2593; MediumShade: \u2592; LightShade: \u2591
-                    playField[i, j].InitOneJewelry('\u2588'); // Dark-Shade: \u2593; MediumShade: \u2592; LightShade: \u2591
-                    //playField[i, j].InitOneJewelry(''); // Dark-Shade: \u2593; MediumShade: \u2592; LightShade: \u2591
-                    playField[i, j].drawBox();
-                }
-            }
-            return playField;
         }
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bejeweled_blitz
+{
+    internal class Settings
+    {
+    }
+}
